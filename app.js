e7a8418f1726aa7cf3b732b4a0526d47d94cd7f5
oume1412 <saranp0rn_1412@hotmail.com>
2019-01-08 13:44:23 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,109 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var fs = require("fs");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(bodyParser());
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+
+app.get("/hello", function(req, res){
+  res.send("Hello");
+});
+
+app.get("/", (req, res) => {
+  res.send("Sad");
+})
+
+app.get('/listUsers', function (req, res) {
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+     console.log( data );
+     res.end( data );
+  });
+})
+
+var user = {
+  "user4" : {
+     "name" : "mohit",
+     "password" : "password4",
+     "profession" : "teacher",
+     "id": 4
+  }
+}
+
+app.post('/addUser', function (req, res) {
+  // First read existing users.
+  console.log(req.body);
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+
+     data = JSON.parse( data );
+     data["user4"] = req.body;
+     console.log( data );
+     res.end( JSON.stringify(data));
+  });
+})
+
+app.get('/:id', function (req, res) {
+  // First read existing users.
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+     var users = JSON.parse( data );
+     var user = users["user" + req.params.id] 
+     console.log( user );
+     res.end( JSON.stringify(user));
+  });
+})
+
+var id = 4;
+
+app.delete('/deleteUser', function (req, res) {
+  // First read existing users.
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+     data = JSON.parse( data );
+     delete data["user" + req.query.id];
+      
+     console.log( data );
+     res.end( JSON.stringify(data));
+  });
+})
+
+
+var server = app.listen(8080, () => {
+  console.log(`Server started on port: 8080`);
+});
+
+module.exports = app;
